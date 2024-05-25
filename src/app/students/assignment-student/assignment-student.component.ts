bcc9b8b8348c0ed48b92eac70db1a2eb24a42e9d
Princia08 <princiaram38@yahoo.com>
2024-05-25 18:28:19 +0300

    show assignment List and max note

---
+++ src/app/students/assignment-student/assignment-student.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-student',
+  standalone: true,
+  imports: [],
+  templateUrl: './assignment-student.component.html',
+  styleUrl: './assignment-student.component.css'
+})
+export class AssignmentStudentComponent {
+
+}
