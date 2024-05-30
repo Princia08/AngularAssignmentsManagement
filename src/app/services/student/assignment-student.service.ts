73c08bf0f40e7066db842fbc459859a7a9c5d732
Steve <ramiandrisoasteve@gmail.com>
2024-05-31 02:02:14 +0300

    pagination my assignements

--- src/app/services/student/assignment-student.service.ts
+++ src/app/services/student/assignment-student.service.ts
@@ -1,30 +1,30 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Assignment} from "../../assignments/assignment.model";
-import {environment} from "../../../environments/environment.development";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Assignment } from '../../assignments/assignment.model';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AssignmentStudentService {
-
   url = environment.apiURL + '/assignments';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
-  getAssignment(): Observable<any[]> {
+  getAssignment(page: number, limit: number): Observable<any> {
     let token = localStorage.getItem('token');
-    return this.http.get<Assignment[]>(this.url + '/me/'+token);
+    return this.http.get<Assignment[]>(
+      this.url + '/me/' + token + '?page=' + page + '&limit=' + limit
+    );
   }
 
   getAssignmentForNote(): Observable<Assignment[]> {
     let token = localStorage.getItem('token');
-    return this.http.get<Assignment[]>(this.url + '/me/'+token);
+    return this.http.get<Assignment[]>(this.url + '/me/' + token);
   }
 
-  getAssignmentDetails( id: string) : Observable<any> {
-    return this.http.get<Assignment>(this.url+'/'+id);
+  getAssignmentDetails(id: string): Observable<any> {
+    return this.http.get<Assignment>(this.url + '/' + id);
   }
 }
--- src/app/students/assignment-student/assignment-student.component.ts
+++ src/app/students/assignment-student/assignment-student.component.ts
@@ -1,53 +1,100 @@
-import {Component, OnInit} from '@angular/core';
-import {AssignmentStudentService} from "../../services/student/assignment-student.service";
-import {Assignment} from "../../assignments/assignment.model";
-import {DatePipe} from "@angular/common";
-import {environment} from "../../../environments/environment.development";
-import {Router} from "@angular/router";
-
+import { Component, OnInit } from '@angular/core';
+import { AssignmentStudentService } from '../../services/student/assignment-student.service';
+import { Assignment } from '../../assignments/assignment.model';
+import { DatePipe } from '@angular/common';
+import { environment } from '../../../environments/environment.development';
+import { Router } from '@angular/router';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-assignment-student',
   standalone: true,
-  imports: [
-    DatePipe
-  ],
+  imports: [DatePipe],
   templateUrl: './assignment-student.component.html',
-  styleUrl: './assignment-student.component.css'
+  styleUrl: './assignment-student.component.css',
 })
 export class AssignmentStudentComponent implements OnInit {
-  assignments: any= [];
+  assignments: any = [];
+  page: number = 1;
+  limit: number = 10;
+  totalPages!: number;
+  nextPage!: number;
+  prevPage!: number;
+  hasNextPage!: boolean;
+  hasPrevPage!: boolean;
 
-  constructor(private router: Router, private assignmentService : AssignmentStudentService) {
-  }
+  constructor(
+    private router: Router,
+    private assignmentService: AssignmentStudentService
+  ) {}
 
   ngOnInit() {
     this.loadMyAssignment();
   }
 
   loadMyAssignment() {
-    this.assignmentService.getAssignment().subscribe((assignments) => {
-      for (let i = 0; i < assignments.length; i++) {
-        this.assignments.push({
-          id: assignments[i]._id,
-          nom: assignments[i].nom,
-          dateDeRendu: assignments[i].dateDeRendu,
-          rendu: assignments[i].rendu,
-          note: assignments[i].note,
+    this.assignmentService
+      .getAssignment(this.page, this.limit)
+      .subscribe((data) => {
+        let assignments = data.assignments;
+        this.totalPages = data.totalPages;
+        this.nextPage = data.nextPage;
+        this.prevPage = data.prevPage;
+        this.hasNextPage = data.hasNextPage;
+        this.hasPrevPage = data.hasPrevPage;
+        this.assignments = [];
+        for (let i = 0; i < assignments.length; i++) {
+          this.assignments.push({
+            id: assignments[i]._id,
+            nom: assignments[i].nom,
+            dateDeRendu: assignments[i].dateDeRendu,
+            rendu: assignments[i].rendu,
+            note: assignments[i].note,
 
-          nomMatiere: assignments[i].idMatiere?.nom,
-          imageMatiere: assignments[i].idMatiere?.image,
+            nomMatiere: assignments[i].idMatiere?.nom,
+            imageMatiere: assignments[i].idMatiere?.image,
 
-          nomProf :  assignments[i].idMatiere?.prof?.prenom + " " + assignments[i].idMatiere?.prof?.nom,
-          prenomProf :  assignments[i].idMatiere?.prof?.prenom,
-          imageProf: assignments[i].idMatiere?.prof?.image
-        });
-      }
-    });
+            nomProf:
+              assignments[i].idMatiere?.prof?.prenom +
+              ' ' +
+              assignments[i].idMatiere?.prof?.nom,
+            prenomProf: assignments[i].idMatiere?.prof?.prenom,
+            imageProf: assignments[i].idMatiere?.prof?.image,
+          });
+        }
+      });
   }
 
   seeDetails(id: string) {
-    this.router.navigate(['/home/assignmentStudent/details', id])
+    this.router.navigate(['/home/assignmentStudent/details', id]);
+  }
+
+  pagePrecedente() {
+    this.page = this.page - 1;
+    this.loadMyAssignment();
+  }
+  pageSuivante() {
+    this.page = this.page + 1;
+    console.log(this.page);
+    this.loadMyAssignment();
+  }
+
+  premierePage() {
+    this.page = 1;
+    this.loadMyAssignment();
+  }
+
+  dernierePage() {
+    this.page = this.totalPages;
+    console.log(this.page);
+    this.loadMyAssignment();
+  }
+
+  // Pour le composant angular material paginator
+  handlePageEvent(event: PageEvent) {
+    this.page = event.pageIndex + 1;
+    this.limit = event.pageSize;
+    this.loadMyAssignment();
   }
 
   protected readonly environment = environment;
